bfe189014d74a1a45e593bddb33bd8d07b3f8ff6
Ravi goswami <zneroravi@gmail.com>
2020-07-29 22:03:02 +0530

    upload

---
+++ day13.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "Ravi Goswami";
+$num1 = 12;
+$num2 = 23;
+$num3 = $num1 + $num2;
+
+//html inside php
+echo "<h1>  hello table </h1>";
+echo "<table>";
+echo "<td> name</td>";
+echo "</table>";
+//echo $name;
+
+header('Refresh: 2; URL=day13a.php');
+
+?>
---
+++ day13a.php
@@ -0,0 +1,23 @@
+<?php
+include 'day13.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>including</title>
+</head>
+<body>
+
+<h1> <u><i> <?php   
+echo $name;
+    ?>
+    	
+    </i> </u></h1>
+<h2>
+	<?php
+echo $num3;
+	?>
+
+</h2>
+</body>
+</html>
